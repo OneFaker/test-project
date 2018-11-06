86efd7f4b487cee927ecfc5cf448e43f48e1492c
OneFaker <940668676@qq.com>
2018-11-06 15:46:58 +0800

    测试

---
+++ src/main/java/utils/TT.java
@@ -0,0 +1,13 @@
+package main.java.utils;
+
+/**
+ * @author zhouwen
+ * @time 2018/11/6 0006
+ * @copyright 2018 hct
+ * @desc
+ */
+public class TT {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
